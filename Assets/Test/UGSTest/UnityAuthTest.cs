01557d1f40a66242095e91f8a7ae89cab199f039
david0305 <david0305mc@gmail.com>
2025-04-25 17:38:52 +0900

    UGS - UGSSave

--- Assets/Test/UGSTest/UnityAuthTest.cs
+++ Assets/Test/UGSTest/UnityAuthTest.cs
@@ -5,6 +5,8 @@ using Unity.Services.Authentication;
 using Cysharp.Threading.Tasks;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using Unity.Services.CloudSave;
 public class UnityAuthTest : MonoBehaviour
 {
     [SerializeField] TMP_InputField idInputField;
@@ -12,6 +14,9 @@ public class UnityAuthTest : MonoBehaviour
 
     [SerializeField] Button signUpButton;
     [SerializeField] Button signInButton;
+
+    [SerializeField] Button saveButton;
+    [SerializeField] Button loadButton;
     private
     async void Awake()
     {
@@ -52,6 +57,14 @@ public class UnityAuthTest : MonoBehaviour
                 return;
             }
             await SignInWithUsernamePasswordAsync(idInputField.text, passwordInputField.text);
+        });
+        saveButton.onClick.AddListener(() =>
+        {
+            SaveSomeData();
+        });
+        loadButton.onClick.AddListener(() =>
+        {
+            LoadSomeData();
         });
     }
 
@@ -147,5 +160,53 @@ public class UnityAuthTest : MonoBehaviour
               Debug.Log("Player session could not be refreshed and expired.");
           };
     }
+    public async void SaveSomeData()
+    {
+        var data = new Dictionary<string, object> { { "key", "someValue" } };
+        await CloudSaveService.Instance.Data.Player.SaveAsync(data);
+    }
+    public async void LoadSomeData()
+    {
+        var savedData = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { "key" });
+
+        Debug.Log("Done: " + savedData["key"]);
+    }
+    private async UniTask<T> RetrieveSpecificData<T>(string key)
+    {
+        try
+        {
+            var results = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { key });
+
+            if (results.TryGetValue(key, out var item))
+            {
+                return item.Value.GetAs<T>();
+            }
+            else
+            {
+                Debug.Log($"There is no such key as {key}!");
+            }
+        }
+        catch (CloudSaveValidationException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveRateLimitedException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveException e)
+        {
+            Debug.LogError(e);
+        }
+
+        return default;
+    }
+}
+[Serializable]
+public class SampleItem
+{
+    public string SophisticatedString;
+    public int SparklingInt;
+    public float AmazingFloat;
 }
 
