8618eed99e9eb097b3580b04fb21192b07e5d76a
david0305 <david0305mc@gmail.com>
2025-04-25 16:32:14 +0900

    UGS - ID/Password SignIn

--- Assets/Test/UGSTest/UnityAuthTest.cs
+++ Assets/Test/UGSTest/UnityAuthTest.cs
@@ -4,8 +4,13 @@ using System;
 using Unity.Services.Authentication;
 using Cysharp.Threading.Tasks;
 using UnityEngine.UI;
+using TMPro;
 public class UnityAuthTest : MonoBehaviour
 {
+    [SerializeField] TMP_InputField idInputField;
+    [SerializeField] TMP_InputField passwordInputField;
+
+    [SerializeField] Button signUpButton;
     [SerializeField] Button signInButton;
     private
     async void Awake()
@@ -19,12 +24,77 @@ public class UnityAuthTest : MonoBehaviour
         {
             Debug.LogException(e);
         }
+
+        signUpButton.onClick.AddListener(async () =>
+        {
+            if (idInputField.text.Length <= 3)
+            {
+                Debug.LogError("id must > 4");
+                return;
+            }
+            if (passwordInputField.text.Length <= 3)
+            {
+                Debug.LogError("password must > 4");
+                return;
+            }
+            await SignUpWithUsernamePasswordAsync(idInputField.text, passwordInputField.text);
+        });
         signInButton.onClick.AddListener(async () =>
         {
-            await SignInAnonymouslyAsync();
+            if (idInputField.text.Length <= 3)
+            {
+                Debug.LogError("id must > 4");
+                return;
+            }
+            if (passwordInputField.text.Length <= 3)
+            {
+                Debug.LogError("password must > 4");
+                return;
+            }
+            await SignInWithUsernamePasswordAsync(idInputField.text, passwordInputField.text);
         });
     }
 
+    async UniTask SignUpWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignUp is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+    async UniTask SignInWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
     async UniTask SignInAnonymouslyAsync()
     {
         try
