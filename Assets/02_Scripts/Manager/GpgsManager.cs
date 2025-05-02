d048c587f5de4f6080aaa32ffa3a3abccf4231d2
david0305 <david0305mc@gmail.com>
2025-05-02 19:00:08 +0900

    gpgs login

--- Assets/02_Scripts/Manager/GpgsManager.cs
+++ Assets/02_Scripts/Manager/GpgsManager.cs
@@ -2,30 +2,114 @@ using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using TMPro;
+using Cysharp.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 public class GpgsManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI logText;
 
 
+    public string Token;
+    public string Error;
 
-    public void LoginGPGS()
+    public void UnlinkGPGS()
     {
-        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        UnlinkGooglePlayGamesAsync("");
 
     }
-
-    internal void ProcessAuthentication(SignInStatus status)
+    public void LoginGPGS()
     {
-        if (status == SignInStatus.Success)
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate((success) =>
         {
-            var displayName = PlayGamesPlatform.Instance.GetUserDisplayName();
-            var userID = PlayGamesPlatform.Instance.GetUserId();
+            if (success == SignInStatus.Success)
+            {
+                Debug.Log("Login with Google Play games successful.");
 
-            logText.text = $"Login Success {displayName}";
-        }
-        else
+                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+                {
+                    Debug.Log("Authorization code: " + code);
+                    Token = code;
+                    // This token serves as an example to be used for SignInWithGooglePlayGames
+                    SignInWithGooglePlayGamesAsync(Token);
+                });
+            }
+            else
+            {
+                Error = "Failed to retrieve Google play games authorization code";
+                Debug.Log("Login Unsuccessful");
+            }
+        });
+    }
+
+    async UniTask SignInWithGooglePlayGamesAsync(string authCode)
+    {
+        try
         {
-            logText.text = $"Login Failed";
+            await AuthenticationService.Instance.SignInWithGooglePlayGamesAsync(authCode);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async UniTask LinkWithGooglePlayGamesAsync(string authCode)
+    {
+        try
+        {
+            await AuthenticationService.Instance.LinkWithGooglePlayGamesAsync(authCode);
+            Debug.Log("Link is successful.");
+        }
+        catch (AuthenticationException ex) when (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+        {
+            // Prompt the player with an error message.
+            Debug.LogError("This user is already linked with another account. Log in instead.");
+        }
+
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async UniTask UnlinkGooglePlayGamesAsync(string idToken)
+    {
+        try
+        {
+            // await AuthenticationService.Instance.UnlinkGooglePlayGamesAsync(idToken);
+            await AuthenticationService.Instance.UnlinkGooglePlayGamesAsync();
+            Debug.Log("Unlink is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
         }
     }
 }
