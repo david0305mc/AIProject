c44a51837ea6ffae86e25acf32c40e23bb77eebe
david0305 <david0305mc@gmail.com>
2025-07-01 17:33:28 +0900

    저장 로직 개선

--- Assets/Test/SaveDataTest/SaveDataTest.cs
+++ Assets/Test/SaveDataTest/SaveDataTest.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Reflection;
 
 using Random = UnityEngine.Random;
+using TMPro;
 
 namespace SaveData
 {
@@ -29,7 +30,6 @@ namespace SaveData
     public class UserData
     {
         public BaseData BaseData = new BaseData();
-        public Dictionary<int, int> SkillLevels = new Dictionary<int, int>();
         public Dictionary<int, int> ItemCount = new Dictionary<int, int>();
         public Dictionary<int, QuestData> QuestData { get; set; } = new Dictionary<int, QuestData>();
 
@@ -54,49 +54,6 @@ namespace SaveData
         private Dictionary<string, bool> dirtyFlags = new Dictionary<string, bool>();
         private string dataPath => Application.persistentDataPath;
 
-        // 각 필드/프로퍼티별로 1개 파일 저장
-        public void SaveAllData()
-        {
-            var members = typeof(UserData)
-                .GetMembers(BindingFlags.Public | BindingFlags.Instance)
-                .Where(m => m.MemberType == MemberTypes.Field || m.MemberType == MemberTypes.Property);
-
-            foreach (var member in members)
-            {
-                Type type;
-                object value;
-                string name = member.Name;
-
-                if (member is FieldInfo field)
-                {
-                    type = field.FieldType;
-                    value = field.GetValue(UserData);
-                }
-                else if (member is PropertyInfo prop && prop.CanRead && prop.CanWrite)
-                {
-                    type = prop.PropertyType;
-                    value = prop.GetValue(UserData);
-                }
-                else
-                {
-                    continue;
-                }
-
-                string fileName = Path.Combine(dataPath, $"{name}.json");
-
-                // ReactiveProperty 처리
-                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ReactiveProperty<>))
-                {
-                    var valueProp = type.GetProperty("Value");
-                    var raw = valueProp.GetValue(value);
-                    File.WriteAllText(fileName, JsonConvert.SerializeObject(raw, Formatting.Indented));
-                }
-                else
-                {
-                    File.WriteAllText(fileName, JsonConvert.SerializeObject(value, Formatting.Indented));
-                }
-            }
-        }
 
         public void LoadAllData()
         {
@@ -155,13 +112,69 @@ namespace SaveData
         {
             UserData.BaseData.userUID = UserData.GetNextUID();
             MarkDirty(nameof(UserData.BaseData));
-            SaveAllData();
+            SaveData(true);
+        }
+
+
+        // 각 필드/프로퍼티별로 1개 파일 저장
+        public void SaveData(bool forceSave = false)
+        {
+            if (!forceSave && dirtyFlags.Count == 0)
+                return;
+
+            var members = typeof(UserData)
+                .GetMembers(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.MemberType == MemberTypes.Field || m.MemberType == MemberTypes.Property);
+
+            foreach (var member in members)
+            {
+                string name = member.Name;
+                if(!forceSave && (!dirtyFlags.ContainsKey(name) || !dirtyFlags[name]))
+                {
+                    continue;
+                }
+
+                Type type;
+                object value;
+
+                if (member is FieldInfo field)
+                {
+                    type = field.FieldType;
+                    value = field.GetValue(UserData);
+                }
+                else if (member is PropertyInfo prop && prop.CanRead && prop.CanWrite)
+                {
+                    type = prop.PropertyType;
+                    value = prop.GetValue(UserData);
+                }
+                else
+                {
+                    continue;
+                }
+
+                string fileName = Path.Combine(dataPath, $"{name}.json");
+
+                // ReactiveProperty 처리
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ReactiveProperty<>))
+                {
+                    var valueProp = type.GetProperty("Value");
+                    var raw = valueProp.GetValue(value);
+                    File.WriteAllText(fileName, JsonConvert.SerializeObject(raw, Formatting.Indented));
+                }
+                else
+                {
+                    File.WriteAllText(fileName, JsonConvert.SerializeObject(value, Formatting.Indented));
+                }
+            }
         }
     }
 
 
     public class SaveDataTest : MonoBehaviour
     {
+
+        [SerializeField] private TextMeshProUGUI saveDataText;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -172,12 +185,13 @@ namespace SaveData
                 Debug.Log("No existing user data found. Creating new user UID.");
                 UserDataManager.Instance.UserData = new UserData();
                 UserDataManager.Instance.CreateNewUser();
-                UserDataManager.Instance.SaveAllData();
             }
             else
             {
                 Debug.Log($"Existing User UID: {userData.BaseData.userUID}");
             }
+            UpdateSaveDataText();
+            UserDataManager.Instance.UserData.BaseData.Gold.Subscribe(_ => UpdateSaveDataText()).AddTo(this);
         }
         public void OnClickAddItem()
         {
@@ -192,24 +206,9 @@ namespace SaveData
                 UserDataManager.Instance.UserData.ItemCount[itemId] = itemCount;
             }
             UserDataManager.Instance.MarkDirty(nameof(UserData.ItemCount));
-            UserDataManager.Instance.SaveAllData();
+            UserDataManager.Instance.SaveData();
             Debug.Log($"Item {itemId} added. New count: {UserDataManager.Instance.UserData.ItemCount[itemId]}");
-        }
-        public void OnClickAddSkill()
-        {
-            int skillId = Random.Range(1, 100);
-            int skillLevel = Random.Range(1, 5);
-            if (UserDataManager.Instance.UserData.SkillLevels.ContainsKey(skillId))
-            {
-                UserDataManager.Instance.UserData.SkillLevels[skillId] += skillLevel;
-            }
-            else
-            {
-                UserDataManager.Instance.UserData.SkillLevels[skillId] = skillLevel;
-            }
-            UserDataManager.Instance.MarkDirty(nameof(UserData.SkillLevels));
-            UserDataManager.Instance.SaveAllData();
-            Debug.Log($"Skill {skillId} added. New level: {UserDataManager.Instance.UserData.SkillLevels[skillId]}");
+            UpdateSaveDataText();
         }
         public void OnClickAddQuest()
         {
@@ -223,20 +222,40 @@ namespace SaveData
                 UserDataManager.Instance.UserData.QuestData[questId] = new QuestData { id = questId, progress = 0, isCompleted = false };
             }
             UserDataManager.Instance.MarkDirty(nameof(UserData.QuestData));
-            UserDataManager.Instance.SaveAllData();
+            UserDataManager.Instance.SaveData();
             Debug.Log($"Quest {questId} added. New progress: {UserDataManager.Instance.UserData.QuestData[questId].progress}");
+            UpdateSaveDataText();
         }
 
         public void OnClickAddGold()
         {
             long goldToAdd = Random.Range(100, 1000);
             UserDataManager.Instance.UserData.BaseData.Gold.Value += goldToAdd;
-            UserDataManager.Instance.MarkDirty(nameof(UserData.BaseData.Gold));
-            UserDataManager.Instance.SaveAllData();
+            UserDataManager.Instance.MarkDirty(nameof(UserData.BaseData));
+            UserDataManager.Instance.SaveData();
             Debug.Log($"Gold added: {goldToAdd}. New total: {UserDataManager.Instance.UserData.BaseData.Gold.Value}");
         }
 
-
+        public void UpdateSaveDataText()
+        {
+            var ud = UserDataManager.Instance.UserData;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine($"UserUID: {ud.BaseData.userUID}");
+            sb.AppendLine($"Gold: {ud.BaseData.Gold.Value}");
+            sb.AppendLine("--- ItemCount ---");
+            foreach (var kv in ud.ItemCount)
+                sb.AppendLine($"ItemID: {kv.Key}, Count: {kv.Value}");
+            sb.AppendLine("--- QuestData ---");
+            foreach (var kv in ud.QuestData)
+            {
+                var q = kv.Value;
+                sb.AppendLine($"QuestID: {q.id}, Progress: {q.progress}, Complete: {q.isCompleted}, CompleteCount: {(q.CompleteCount != null ? q.CompleteCount.Value.ToString() : "null")}");
+            }
+            if (saveDataText != null)
+                saveDataText.text = sb.ToString();
+            else
+                Debug.Log(sb.ToString());
+        }
     }
 
 
